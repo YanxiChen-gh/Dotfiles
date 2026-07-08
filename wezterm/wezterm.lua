4b87babcd48562af841ef60c9049dca1e7232bd9
Yanxi Chen <13044821+YanxiChen-gh@users.noreply.github.com>
2026-07-08 20:13:18 +0000

    Enable Command-click links in WezTerm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,6 +10,15 @@ config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 -- config.window_decorations = "RESIZE"
 
+config.bypass_mouse_reporting_modifiers = "CMD"
+config.mouse_bindings = {
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CMD",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
+}
+
 config.keys = {
 	-- cmd+backspace deletes to the start of the line (readline Ctrl-U)
 	{
