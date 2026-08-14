50f53a6c0ef89376ff7de8a29a4aab8dede6c6cc
Yanxi Chen <13044821+YanxiChen-gh@users.noreply.github.com>
2026-08-14 17:00:01 +0000

    Let Herdr handle modified link clicks

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,7 +10,6 @@ config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 -- config.window_decorations = "RESIZE"
 
-config.bypass_mouse_reporting_modifiers = "CMD"
 config.mouse_bindings = {
 	{
 		event = { Up = { streak = 1, button = "Left" } },
